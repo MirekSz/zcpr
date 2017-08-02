b7186b8f22a2cf9d9351c909d20cd1c037e5f7a5
MirekSz <miro1994>
2017-08-02 15:53:29 +0200

    route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    HashRouter as Router,
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {About, UserDetails, Users} from './Trash';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Router>
+        <div className="container">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/users">Users</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/about" component={About}/>
+                <Route path="/users" component={Users}/>
+            </Switch>
+        </div>
+    </Router>
+), document.getElementById('root'));
 registerServiceWorker();
